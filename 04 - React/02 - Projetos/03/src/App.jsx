f1875f44aee02d3ebaebc44a2473aad98e942be3
Alleph Nogueira <126208329+AllephNogueira@users.noreply.github.com>
2026-01-18 00:19:52 -0300

    Criando estilos no react

---
+++ 04 - React/02 - Projetos/03/src/App.jsx
@@ -0,0 +1,17 @@
+//Style
+import './App.css'
+
+// Componentes
+import PrimeiroComponente from './componentes/PrimeiroComponente.jsx' 
+
+function App() {
+
+  return (
+    <div>
+        <h1>Criando componentes</h1>
+        <PrimeiroComponente />
+        </div>
+  )
+}
+
+export default App
---
+++ 04 - React/02 - Projetos/03/src/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,10 @@
+const PrimeiroComponente = () => {
+    return (
+        <div>
+            <h1 className="texto-grande">Primeiro Componente criado</h1>
+            <p className="texto-pequeno">Aprensetando meu primeiro Componente</p>
+        </div>
+    )
+}
+
+export default PrimeiroComponente;
