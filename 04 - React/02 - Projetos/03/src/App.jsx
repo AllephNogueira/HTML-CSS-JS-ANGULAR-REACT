4d558ebde0ce575690814fbf709a4890a2efa5e1
Alleph Nogueira <126208329+AllephNogueira@users.noreply.github.com>
2026-01-18 00:27:36 -0300

    TempleteExpressions

--- 04 - React/02 - Projetos/03/src/App.jsx
+++ 04 - React/02 - Projetos/03/src/App.jsx
@@ -3,13 +3,16 @@ import './App.css'
 
 // Componentes
 import PrimeiroComponente from './componentes/PrimeiroComponente.jsx' 
+import TempleteExpressions from './componentes/TempleteExpressions.jsx'
 
 function App() {
+
 
   return (
     <div>
         <h1>Criando componentes</h1>
         <PrimeiroComponente />
+        <TempleteExpressions />
         </div>
   )
 }
---
+++ 04 - React/02 - Projetos/03/src/componentes/TempleteExpressions.jsx
@@ -0,0 +1,12 @@
+const TempleteExpressions = () => {
+
+    const nome = "Alleph"
+
+    return (
+        <div>
+            <h1>Ola {nome} tudo bem?</h1>
+        </div>
+    )
+}
+
+export default TempleteExpressions
